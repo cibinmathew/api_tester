23807e4aa34ad44620f343f2bf60acfe4b279155
cibinmathew <ikonland@gmail.com>
2018-07-06 23:29:34 +0530

    configurable settings

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 import json
 import os
 import requests
-url="https://jsonplaceholder.typicode.com/posts/1"
 
-response_dir=r"requests"
-request_dir=r"requests"
+url          = "https://jsonplaceholder.typicode.com/posts/1"
+response_dir = r"responses"
+request_dir  = r"requests"
 
-def ordered(obj):
+def sort_dict(obj):
     if isinstance(obj, dict):
-        return sorted((k, ordered(v)) for k, v in obj.items())
+        return sorted((k, sort_dict(v)) for k, v in obj.items())
     if isinstance(obj, list):
-        return sorted(ordered(x) for x in obj)
+        return sorted(sort_dict(x) for x in obj)
     else:
         return obj
 
@@ -21,12 +21,15 @@ for file in os.listdir(os.fsencode(request_dir)):
         response= json.loads(f.read())
     with open(os.path.join(request_dir, filename)) as f:
         request= f.read()
-        print(request)
         # continue
     # else:
         # continue
     resp = json.loads(requests.get(url).text)
-    if ordered(resp.items()) == ordered(response.items()):
-        print("passed")
+    print(resp)
+    print(sort_dict(resp.items()))
+    if sort_dict(resp.items()) == sort_dict(response.items()):
+        # print(request)
+        print("{} passed".format(filename))
     else:
-        print("Faliled")
+        print(sort_dict(response.items()))
+        print("{} failed".format(filename))
